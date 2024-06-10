d88ee55d8bb0486f593621cccf538487295bb32e
Maisha <maisha.mir2014@gmail.com>
2024-06-09 22:38:53 -0700

    styled comments for tablet and mobile

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,21 +1,22 @@
 import "./Comment.scss";
 
 function Comment({ commentObj }) {
-  // console.log("Current comment => ", commentObj);
   return (
     <>
-      <div className="comment" >
+      <div className="comment">
         <div className="comment__avatar"></div>
-        <div className="comment__stats">
-          <p className="comment__author">{commentObj.name}</p>
-          <p className="comment__date">{new Date(commentObj.timestamp).toLocaleDateString('en-US') }</p>
+        <div class="comment__content">
+          <div className="comment__stats">
+            <p className="comment__author">{commentObj.name}</p>
+            <p className="comment__date">
+              {new Date(commentObj.timestamp).toLocaleDateString("en-US")}
+            </p>
+          </div>
+          <p className="comment__text">{commentObj.comment}</p>
         </div>
-        <p className="comment__text">{ commentObj.comment}</p>
       </div>
-      <hr />
+      <hr className="comment-divider"/>
     </>
-
-    // <h1>Comment</h1>
   );
 }
 
