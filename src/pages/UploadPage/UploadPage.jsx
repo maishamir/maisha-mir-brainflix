c55624ca85f9de0da35d155d294bb2358dfac26d
Maisha <maisha.mir2014@gmail.com>
2024-06-16 22:20:32 -0700

    added styling for form errors

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -5,72 +5,101 @@ import { Link, Navigate } from "react-router-dom";
 
 function UploadPage() {
   const [redirect, setRedirect] = useState(false);
-  const [buttonText, setButtonText] = useState("PUBLISH")
+  const [buttonText, setButtonText] = useState("PUBLISH");
   const formRef = useRef();
+  const titleRef = useRef();
+  const descRef = useRef();
 
   function handleSubmit() {
-    const form = formRef.current
-    console.log(formRef.current)
+    const form = formRef.current;
+    const title = titleRef.current.value;
 
-    setButtonText('UPLOADING...')
+    const desc = descRef.current.value;
+    console.log(title, desc)
+    
+    if (!title || !desc) {
+      if (!title) {
+        titleRef.current.classList.add("upload-page__error");
+        titleRef.current.focus()
+      }
+      if (!desc) {
+        descRef.current.classList.add("upload-page__error");
+        if (title) {
+          descRef.current.focus();
+        }
+      }
+      return
+    }
+
+
+    setButtonText("UPLOADING");
     form.reset();
 
     setTimeout(() => {
-      setButtonText('POSTED')
+      setButtonText("POSTED");
       setTimeout(() => {
-        setRedirect(true)
-      }, 1000)
-    }, 2000)
+        setRedirect(true);
+      }, 1000);
+    }, 2000);
   }
 
   if (redirect) {
     return <Navigate to="/" />;
   }
 
-
   return (
     <main className="upload-page">
       <h1 className="upload-page__title">Upload Video</h1>
-
-      <hr className="upload-page__divider"/>
-      <h2 className="upload-page__label">VIDEO THUMBNAIL</h2>
-      <img src={image} alt="" className="upload-page__thumbnail" />
-
-      <form className="upload-page__form" ref={formRef}>
-        <div className="upload-page__input">
-          <label htmlFor="videoTitle" className="upload-page__form-label">
-            TITLE YOUR VIDEO
-          </label>
-          <br />
-          <input
-            className="upload-page__vid-title"
-            type="text"
-            name="videoTitle"
-            id="videoTitle"
-            placeholder="Add a title to your video"
-          />
-        </div>
-        <div className="upload-page__input">
-          <label htmlFor="videoDesc">ADD A VIDEO DESCRIPTION</label>
-          <br />
-          <textarea
-            name="videoDesc"
-            id="videoDesc"
-            className="upload-page__vid-desc"
-            placeholder="Add a description to your video"
-          />
+      <hr className="upload-page__divider" />
+      <div className="upload-page__container">
+        <div className="upload-page__video">
+          <h2 className="upload-page__label">VIDEO THUMBNAIL</h2>
+          <img src={image} alt="" className="upload-page__thumbnail" />
         </div>
 
-        <hr className="upload-page__divider"/>
-        <div className="upload-page__button-container">
-          <button type="button" className="upload-page__publish" onClick={handleSubmit}>{buttonText}</button>
-          <Link to="/" className="upload-page__cancel">
-            CANCEL
-          </Link>
+        <div className="upload-page__form-container">
+          <form className="upload-page__form" ref={formRef}>
+            <div className="upload-page__input">
+              <label htmlFor="videoTitle" className="upload-page__form-label">
+                TITLE YOUR VIDEO
+              </label>
+              <br />
+              <input
+                ref={titleRef}
+                className="upload-page__vid-title"
+                type="text"
+                name="videoTitle"
+                id="videoTitle"
+                placeholder="Add a title to your video"
+              />
+            </div>
+            <div className="upload-page__input">
+              <label htmlFor="videoDesc">ADD A VIDEO DESCRIPTION</label>
+              <br />
+              <textarea
+                ref = {descRef}
+                name="videoDesc"
+                id="videoDesc"
+                className="upload-page__vid-desc"
+                placeholder="Add a description to your video"
+              />
+            </div>
+          </form>
         </div>
-
-
-      </form>
+      </div>
+      <hr className="upload-page__divider" />
+      <div className="upload-page__button-container">
+        <button
+          type="button"
+          className="upload-page__publish"
+          onClick={handleSubmit}
+        >
+          {buttonText}
+        </button>
+        <Link to="/" className="upload-page__cancel">
+          CANCEL
+        </Link>
+      </div>
     </main>
   );
 }
