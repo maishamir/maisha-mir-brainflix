53d086181037f6a805715958d0d1a3fdabe6d10e
Maisha <maisha.mir2014@gmail.com>
2024-06-16 19:07:35 -0700

    styled page for tablet; added state for successful upload

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,32 +1,42 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import image from "../../assets/images/Upload-video-preview.jpg";
 import "./UploadPage.scss";
 import { Link, Navigate } from "react-router-dom";
 
 function UploadPage() {
   const [redirect, setRedirect] = useState(false);
+  const [buttonText, setButtonText] = useState("PUBLISH")
+  const formRef = useRef();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const form = e.target;
+  function handleSubmit() {
+    const form = formRef.current
+    console.log(formRef.current)
 
-    alert("Uploaded successfully!");
+    setButtonText('UPLOADING...')
     form.reset();
 
-    setRedirect(true);
+    setTimeout(() => {
+      setButtonText('POSTED')
+      setTimeout(() => {
+        setRedirect(true)
+      }, 1000)
+    }, 2000)
   }
 
   if (redirect) {
     return <Navigate to="/" />;
   }
+
+
   return (
     <main className="upload-page">
       <h1 className="upload-page__title">Upload Video</h1>
 
+      <hr className="upload-page__divider"/>
       <h2 className="upload-page__label">VIDEO THUMBNAIL</h2>
       <img src={image} alt="" className="upload-page__thumbnail" />
 
-      <form className="upload-page__form" onSubmit={handleSubmit}>
+      <form className="upload-page__form" ref={formRef}>
         <div className="upload-page__input">
           <label htmlFor="videoTitle" className="upload-page__form-label">
             TITLE YOUR VIDEO
@@ -50,8 +60,10 @@ function UploadPage() {
             placeholder="Add a description to your video"
           />
         </div>
+
+        <hr className="upload-page__divider"/>
         <div className="upload-page__button-container">
-          <Link to="/" className="upload-page__publish button-link" >PUBLISH</Link>
+          <button type="button" className="upload-page__publish" onClick={handleSubmit}>{buttonText}</button>
           <Link to="/" className="upload-page__cancel">
             CANCEL
           </Link>
