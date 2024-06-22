89e3f6c22c69c8efd731cb62422451ef33049f0a
Maisha <maisha.mir2014@gmail.com>
2024-06-22 10:58:23 -0700

    fixe useEffect to make less API calls

--- src/pages/VideoDetailsPage/VideoDetailsPage.jsx
+++ src/pages/VideoDetailsPage/VideoDetailsPage.jsx
@@ -1,12 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import VideoSection from "../../components/VideoSection/VideoSection.jsx";
 import API_URL, { API_KEY } from "../../utils.js";
-import VideoInfo from "../../components/VideoInfo/VideoInfo.jsx";
-import VideoDescription from "../../components/VideoDescription/VideoDescription.jsx";
-import CommentSection from "../../components/CommentSection/CommentSection.jsx";
-import Sidebar from "../../components/Sidebar/Sidebar.jsx";
 import MainSection from "../../components/MainSection/MainSection.jsx";
 
 function VideoDetailsPage() {
@@ -14,60 +9,33 @@ function VideoDetailsPage() {
   const [videoDetailsData, setVideoDetailsData] = useState(null); // State to store the details of the current video
   const [videoList, setVideolist] = useState([]); // State to store the list of other videos for the sidebar
 
-  // Fetch video details and list of videos whenever videoId changes
   useEffect(() => {
-    const fetchVideoDetails = async () => {
-      let vidId = ""; // Variable to store the current video ID
+    const fetchVideoData = async () => {
       try {
-        if (videoId === undefined || videoId === null) {
-          // If no specific video ID is provided, fetch the default video details
-          const videoResponse = await axios.get(`${API_URL}videos${API_KEY}`);
-          vidId = videoResponse.data[0].id;
-
-          const defaultVideoDetail = await axios.get(
-            `${API_URL}videos/${vidId}${API_KEY}`
-          );
-          setVideoDetailsData(defaultVideoDetail.data); // Set the default video details
+        const allVids = await axios.get(`${API_URL}videos${API_KEY}`);
+        if (videoId) {
+          const {data} = await axios.get(`${API_URL}videos/${videoId}${API_KEY}`);
+          setVideoDetailsData(data)
         } else {
-          // Fetch details of the specified video by ID
-          vidId = videoId;
-          const videoResponse = await axios.get(
-            `${API_URL}videos/${vidId}${API_KEY}`
-          );
-          setVideoDetailsData(videoResponse.data); // Set the specified video details
+          const id = allVids.data[0].id;
+          const {data} = await axios.get(`${API_URL}videos/${id}${API_KEY}`);
+          setVideoDetailsData(data)
         }
-
-        // Fetch the list of all videos
-        const vidsResponse = await axios.get(`${API_URL}videos${API_KEY}`);
-        const vidsResponseData = vidsResponse.data;
-
-        // Filter out the current video from the list of videos
-        const remainingVideos = vidsResponseData.filter(
-          (video) => video.id !== vidId
-        );
-
-        // Set the sidebar videos
-        setVideolist(remainingVideos);
-      } catch (err) {
-        console.error("Error", err); // Log any errors during the data fetching process
+        const sidebarVideos = allVids.data.filter(video => video.id !== videoId)
+        setVideolist(sidebarVideos)
+      } catch (e) {
+        console.log(e);
       }
-    };
-    fetchVideoDetails(); // Call the function to fetch video details
-  }, [videoId]); // The useEffect will run whenever the videoId changes
+    }
+    fetchVideoData()
+  }, [videoId]);
 
   // If video details or video list are not yet loaded, display a loading message
   if (!videoDetailsData || videoList.length === 0) {
     return <p>Just a moment while we load the video details...</p>;
   }
 
-  // Destructure properties from videoDetailsData for easier access
-  const { title, channel, image, description, comments } = videoDetailsData;
-
-  return (
-
-      <MainSection currentVideo={videoDetailsData} videos={videoList } />
-
-  );
+  return <MainSection currentVideo={videoDetailsData} videos={videoList} />;
 }
 
-export default VideoDetailsPage;
+export default VideoDetailsPage
