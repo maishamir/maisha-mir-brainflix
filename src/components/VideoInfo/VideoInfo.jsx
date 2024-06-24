199c9e6f330b24609ff5b2d38ee83c0af5dcc04e
Maisha <maisha.mir2014@gmail.com>
2024-06-23 19:09:43 -0700

    icons are now accessed from static folder

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -1,4 +1,5 @@
 import "./VideoInfo.scss";
+const API_URL = import.meta.env.VITE_API_URL
 
 function VideoInfo({ currentVideo }) {
   return (
@@ -8,13 +9,17 @@ function VideoInfo({ currentVideo }) {
       
       <div className="video-info__container">
         <div className="video-info__meta">
-        <p className="video-info__channel">By {currentVideo.channel}</p>
-        <p className="video-info__date">{new Date(currentVideo.timestamp).toLocaleDateString()}</p>
-      </div>
-      <div className="video-info__stats">
-        <p className="video-info__views"><img src="src/assets/icons/views.svg" alt="" className="video-info__logo"/> {currentVideo.views}</p>
-        <p className="video-info__likes"><img src="src/assets/icons/likes.svg" alt="" className="video-info__logo"/> {currentVideo.likes}</p>
-      </div>
+          <p className="video-info__channel">By {currentVideo.channel}</p>
+          <p className="video-info__date">{new Date(currentVideo.timestamp).toLocaleDateString()}</p>
+        </div>
+        <div className="video-info__stats">
+          <p className="video-info__views">
+            <img src={`${API_URL}images/icons/views.svg`} alt="Views" className="video-info__logo" /> {currentVideo.views}
+          </p>
+          <p className="video-info__likes">
+            <img src={`${API_URL}images/icons/likes.svg`} alt="Likes" className="video-info__logo" /> {currentVideo.likes}
+          </p>
+        </div>
       </div>
       
       <hr className="video-info__divider"/>
